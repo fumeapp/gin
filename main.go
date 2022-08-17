6c54452fd185456ab14f63aef5aa942e90e0103e
kevin olson <acidjazz@gmail.com>
2022-08-17 16:53:35 -0500

    ✨ initial commit

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	ginadapter "github.com/awslabs/aws-lambda-go-api-proxy/gin"
+	"github.com/gin-gonic/gin"
+)
+
+var ginLambda *ginadapter.GinLambdaV2
+
+type Options struct {
+	dev bool // development mode
+	port int // port to run on (default 8080)
+}
+
+func main() {}
+
+func Start(routes *gin.Engine, options *Options) {
+
+	defaults := &Options{
+		dev: false,
+		port: 8080,
+	}
+
+	if options.dev {
+		defaults.dev = options.dev
+	}
+
+	if options.port != 0 {
+		defaults.port = options.port
+	}
+
+	if options.dev {
+		server := &http.Server{
+			Addr:    fmt.Sprintf(":%d", defaults.port),
+			Handler: routes,
+		}
+		server.ListenAndServe()
+	} else {
+		ginLambda = ginadapter.NewV2(routes)
+		lambda.Start(Handler)
+	}
+}
+
+func Handler(ctx context.Context, req events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
+	return ginLambda.ProxyWithContext(ctx, req)
+}
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Fume adapter for Gin
+
+[![Go Reference][1]][2]
+[![GoCard][3]][4]
+
+
+[1]: https://pkg.go.dev/badge/github.com/aws/aws-lambda-go.svg
+[2]: https://pkg.go.dev/github.com/aws/aws-lambda-go
+[3]: https://goreportcard.com/badge/github.com/aws/aws-lambda-go
+[4]: https://goreportcard.com/report/github.com/aws/aws-lambda-go
